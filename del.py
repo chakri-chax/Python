db2549342455ad44fcdc41f37ead678772d526e8
chakri-chax <lovachakravarthi@gmail.com>
2023-05-09 01:01:15 +0530

    ..

--- del.py
+++ del.py
@@ -1,24 +1,30 @@
-# # Aniyahs_list = ["Jacomo", "Emma", "Uli", "Nia", "Imani"]
-# # Imanis_list = ["Loik", "Gabriel", "Ahmed", "Soraya"]
-# # print(Aniyahs_list + Imanis_list)
-# list1 = ["Jacomo", "Emma", "Uli", "Nia", "Imani"]
-# list2 = ["Loik", "Gabriel", "Ahmed", "Soraya"]
-# new_list = list1+list2 # Initialize a new list.
-# combine_list = new_list# Combine the lists.
-# combine_list.sort() # Sort the combined lists.
-# new_list = combine_list  # Assign the combined lists to the "new_list".
-# print(new_list)
+# # # Aniyahs_list = ["Jacomo", "Emma", "Uli", "Nia", "Imani"]
+# # # Imanis_list = ["Loik", "Gabriel", "Ahmed", "Soraya"]
+# # # print(Aniyahs_list + Imanis_list)
+# # list1 = ["Jacomo", "Emma", "Uli", "Nia", "Imani"]
+# # list2 = ["Loik", "Gabriel", "Ahmed", "Soraya"]
+# # new_list = list1+list2 # Initialize a new list.
+# # combine_list = new_list# Combine the lists.
+# # combine_list.sort() # Sort the combined lists.
+# # new_list = combine_list  # Assign the combined lists to the "new_list".
+# # print(new_list)
 
-# # # Python program to sort a list of strings
+# # # # Python program to sort a list of strings
 
-# # lst = ['gfg', 'is', 'a', 'portal', 'for', 'geeks']
+# # # lst = ['gfg', 'is', 'a', 'portal', 'for', 'geeks']
 
-# # # Using sort() function
-# # lst.sort()
+# # # # Using sort() function
+# # # lst.sort()
 
-# # print(lst)
-# A Kia Soul costs 19000 dollars
-kia = "Hello"
-price = 19000
-result = "A "+ kia +" costs " +str(price) +" dollars"
-print(result)
+# # # print(lst)
+# # A Kia Soul costs 19000 dollars
+# kia = "Hello"
+# price = 19000
+# result = "A "+ kia +" costs " +str(price) +" dollars"
+# print(result)
+
+text = "hello2"
+for i in range(len(text)):
+    key = text[i]
+    if type(key) != str:
+        print(key)
---
+++ task7.py
@@ -0,0 +1,31 @@
+def count_letters(text):
+  # Initialize a new dictionary.
+  dictionary = {} 
+  # Complete the for loop to iterate through each "text" character and 
+  # use a string method to ensure all letters are lowercase.
+  for i in range(len(text)):   
+    # Complete the if-statement using a string method to check if the
+    # character is a letter.
+    key = text[i]
+    if key == str:
+
+      # Complete the if-statement using a logical operator to check if 
+      # the letter is not already in the dictionary.
+      if _ 
+           # Use a dictionary operation to add the letter as a key
+           # and set the initial count value to zero.
+           _  
+      # Use a dictionary operation to increment the letter count value 
+      # for the existing key.
+      _  
+      _ # Increment the letter counter. 
+  return dictionary
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
