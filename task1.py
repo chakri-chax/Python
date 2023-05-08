6b276c7d8ccef7c30720d208f72d83d69000f272
chakri-chax <lovachakravarthi@gmail.com>
2023-05-08 23:40:00 +0530

    .

--- task1.py
+++ task1.py
@@ -3,6 +3,7 @@ def first_character(string):
     return string[0]; 
 
 
+
 print(first_character("Hello, World")) # Should print H
 print(first_character("Python is awesome")) # Should print P
 print(first_character("Keep going")) # Should print K
