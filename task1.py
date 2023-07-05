e1ff6b139d553b21272128bae2827260101d3ddb
D . L . CHAKRAVARTHI <72123758+chakri-chax@users.noreply.github.com>
2023-07-05 13:11:34 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ def first_character(string):
     # Complete the return statement using a string operation.
     return string[0]; 
 
-
+ 
 
 print(first_character("Hello, World")) # Should print H
 print(first_character("Python is awesome")) # Should print P
